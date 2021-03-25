b78f5e3d5afb9654417483b47ef1f398efeccf03
Kate Cheng <ytcheng2013@gmail.com>
2021-03-25 14:34:43 +0800

    refine version

--- password_retry.py
+++ password_retry.py
@@ -8,11 +8,15 @@
 password = 'a123456'
 i = 3 #剩餘機會
 while i > 0:
+	i = i -1
 	keyin = input('請輸入密碼: ')
 	if keyin == password:
 		print('登入成功!')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有 ', i, ' 次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有 ', i, ' 次機會')
+		else:
+			print('沒有機會嘗試了! 要鎖帳號了啦')
 		
